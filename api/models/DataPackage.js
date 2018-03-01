a8f68bfac481d5d4f0917f29aa542ded13d85e07
JorgeGaete <jgaete83@gmail.com>
2018-03-01 15:34:11 -0300

    EDA added

--- api/models/DataPackage.js
+++ api/models/DataPackage.js
@@ -6,7 +6,7 @@ var DatapackageSchema = new Schema({
   id: {type: String,required: 'Please enter the package ID'},
   user_id: {type: String,required: 'Please enter user ID'},
   package_timestamp: {type: String,required: 'Please enter package timestamp'},
-  sensor: {type: [{type: String,enum: ['ppg','inertial','temp_eda']}]},
+  sensor: {type: [{type: String,enum: ['ppg','inertial','eda']}]},
   package_data: {type: String,default: 'NA'},
   Created_at: {type: Date,defauts: Date.now}
 });
